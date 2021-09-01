212ea1c49c80770039aa3243044d1096d8e646cb
William Nogueira <will.ribeiro@live.com>
2021-09-01 17:12:14 -0300

    Consumindo API - Configurando o MirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions") // buscar dados numa possivel rota que pode ser alterado
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
+
   return (
     <Container>
       <table>
