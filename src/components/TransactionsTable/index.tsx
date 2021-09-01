850d4fe1b893c2fba17938913258669733cc6262
William Nogueira <will.ribeiro@live.com>
2021-09-01 17:46:33 -0300

    Consumindo API - Configurando cliente do Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch("http://localhost:3000/api/transactions") // buscar dados numa possivel rota que pode ser alterado
-    .then(response => response.json())
-    .then(data => console.log(data))
+    api.get('transactions')
+    .then(response => console.log(response.data))
   }, [])
 
   return (
