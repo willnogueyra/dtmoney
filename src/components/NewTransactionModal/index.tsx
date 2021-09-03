50af2509e87ab128593995548dd04b332e6645ae
William Nogueira <will.ribeiro@live.com>
2021-09-03 17:38:27 -0300

    Modal e Forms - Criando botões de Tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg"
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -27,6 +29,19 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
           type="number"
           placeholder="Valor"
         />
+
+        <TransactionTypeContainer>
+          <button>
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button>
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input 
           placeholder="Categoria"
         />
